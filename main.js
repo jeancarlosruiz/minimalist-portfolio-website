eb470cc36826e3dd114fd5600c8396f7cdf227d0
Jean Carlos Ruiz <66891780+jeancarlosruiz@users.noreply.github.com>
2023-02-27 19:46:31 -0400

    Some functionalities with Javascript.

--- main.js
+++ main.js
@@ -3,6 +3,13 @@
 // SELECTING ELEMENTS //
 ///////////////////////
 
+// Navegation mouseover element
+const nav = document.querySelector('.header-nav');
+
+// Smoth Scrolling
+const aboutMeBtn = document.querySelector('.scroll-btn');
+const section1 = document.querySelector('#section1');
+
 // Hamburger elements
 const hamburgerBtn = document.querySelector('.hamburger-btn');
 const headerNav = document.querySelector('.header-nav');
@@ -10,6 +17,19 @@ const headerNav = document.querySelector('.header-nav');
 //////////////////////////////////////
 // FUNCTIONS TO DO NOT REPEAT CODE //
 ////////////////////////////////////
+
+// opacity hover the nav function.
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav-link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav-link');
+    const logo = link.closest('.nav').querySelector('.logo');
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
 
 // Hamburger function
 const toggleClasses = function () {
@@ -21,6 +41,10 @@ const toggleClasses = function () {
 // ADDEVENT FUNCTIONALITIES //
 ///////////////////////////
 
+// Navegation addEventListener
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
+
 // Hamburger addEventListener
 hamburgerBtn.addEventListener('click', () => {
   toggleClasses();
@@ -30,4 +54,10 @@ document.addEventListener('keydown', e => {
   if (e.key === 'Escape' && hamburgerBtn.classList.contains('active')) {
     toggleClasses();
   }
+});
+
+// Scroll
+aboutMeBtn.addEventListener('click', e => {
+  e.preventDefault();
+  section1.scrollIntoView({ behavior: 'smooth' });
 });
