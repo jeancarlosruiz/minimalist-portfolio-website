594f9fd70600b0b3603b2d6c259292bda0ad417e
Jean Carlos Ruiz <66891780+jeancarlosruiz@users.noreply.github.com>
2023-03-02 15:16:43 -0400

    contact form js done

--- main.js
+++ main.js
@@ -17,17 +17,101 @@ const section1 = document.querySelector('#section1');
 const hamburgerBtn = document.querySelector('.hamburger-btn');
 const headerNav = document.querySelector('.header-nav');
 
+// Contact me form
+
+const form = document.getElementsByTagName('form')[0];
+let userName = document.getElementById('userName');
+let email = document.getElementById('mail');
+let textArea = document.getElementById('msg');
+let submitBTN = document.querySelector('.submit-btn');
+const inputs = document.querySelectorAll('.input-control-input');
+const output = document.querySelector('.output');
+
+submitBTN.disabled = true;
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  checkInput();
+});
+
 //////////////////////////////////////
 // FUNCTIONS TO DO NOT REPEAT CODE //
 ////////////////////////////////////
+
+function checkInput() {
+  let userNameValid = userName.value.trim();
+  let emailValid = email.value.trim();
+  let textAreaValid = textArea.value.trim();
+
+  const inputs = document.querySelectorAll('.input-control-input');
+
+  if (userNameValid === '') {
+    setErrorFor(userName, 'This field is required');
+  } else {
+    setSucessFor(userName);
+  }
+  if (textAreaValid === '') {
+    setErrorFor(textArea, 'This field is required');
+  } else {
+    setSucessFor(textArea);
+  }
+  if (emailValid === '') {
+    setErrorFor(email, 'This field is required');
+  } else if (!isEmail(emailValid)) {
+  } else {
+    setSucessFor(email);
+  }
+  if (userNameValid !== '' && emailValid !== '' && textAreaValid !== '') {
+    inputs.forEach(singleInput => (singleInput.value = ''));
+    output.innerHTML = `Thank you ${userNameValid}, your request was successfully submitted`;
+    submitBTN.disabled = true;
+  } else {
+    output.textContent = '';
+  }
+}
+function setErrorFor(input, message) {
+  const formControl = input.parentElement;
+  const error = formControl.querySelector('.error');
+  error.style.display = 'block';
+  error.innerText = message;
+  input.style.border = '2px solid hsl(0, 90%, 57%)';
+}
+
+function setSucessFor(input) {
+  const formControl = input.parentElement;
+  const error = formControl.querySelector('.error');
+  error.style.display = 'none';
+  input.style.border = '2px solid hsl(167, 36%, 54%)';
+}
+
+function isEmail(email) {
+  return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+    email
+  );
+}
+
+// Btn disable functionality
+
+inputs.forEach(singleInput =>
+  singleInput.addEventListener('change', () => {
+    if (singleInput.value === '') {
+      submitBTN.disabled = true;
+    } else {
+      submitBTN.disabled = false;
+      submitBTN.style.backgroundColor = 'hsl(205, 41%, 21%)';
+      submitBTN.style.color = 'hsl(0, 0%, 98%)';
+    }
+  })
+);
 
 // Revealing section
 
 const revealSection = function (entries, oberserver) {
   const [entry] = entries;
-  console.log(entry);
 
   if (!entry.isIntersecting) return;
+
   entry.target.classList.remove('section-hidden');
   oberserver.unobserve(entry.target);
 };
